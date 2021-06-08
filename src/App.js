38763d1834ec74ad2644343205aedb58905d6291
danpinon <danielpformoso@gmail.com>
2021-06-07 22:12:26 -0500

    begining front

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { BrowserRouter  as Router, Route } from 'react-router-dom'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import Home from './components/Home'
+function App() {
+  return (
+    <Router>
+      <>
+        <Header/>
+        <div className="container container-fluid">
+          <Route 
+            path = "/"
+            component={Home}
+            exact
+            />
+        </div>
+        <Footer/>
+      </>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import MetaData from "./layout/MetaData";
+
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../actions/productActions";
+
+import Product from './product/Product'
+
+const Home = () => {
+  const dispatch = useDispatch();
+
+  const { loading, products, error, productsCount } = useSelector(
+    (state) => state.products
+  );
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  return (
+    <>
+      {loading? <h1>Loading...</h1> : (
+        <>
+        <MetaData title={"Buy Best Products Online"} />
+
+        <h1 id="products_heading">Latest Products</h1>
+
+        <section id="products" className="container mt-5">
+          <div className="row">
+            {products &&
+              products.map((product) => (
+                <Product key={product._id} product={product}/>
+              ))}
+          </div>
+        </section>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Home;
